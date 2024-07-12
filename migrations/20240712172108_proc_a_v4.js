b1d065f973d3a6416833b0ebe7b10e4b84994cd7
hanseltime <hanseltime@gmail.com>
2024-07-12 11:22:39 -0600

    test: simulating a new change to a stored_procedure

---
+++ migrations/20240712172108_proc_a_v4.js
@@ -0,0 +1,16 @@
+exports.up = async (knex) => {
+    // This fundamentally changes the schema and returns of the procedure
+    // This is an example of something we want to explicitly catch
+    await knex.raw(`
+    CREATE OR REPLACE PROCEDURE "SelectAllDiscisples"
+    AS
+    BEGIN
+    SELECT * FROM kungfu_disciple WHERE id = 99;
+    END;
+`)
+}
+
+
+exports.down = async (knex) => {
+}
+
